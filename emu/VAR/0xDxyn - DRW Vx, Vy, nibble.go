227a93597b6c4f18ae7c9efed7b7155771b22517
BuildTools <unconfigured@null.spigotmc.org>
2023-10-05 15:52:41 +0200

    800fps

--- emu/VAR/0xDxyn - DRW Vx, Vy, nibble.go
+++ emu/VAR/0xDxyn - DRW Vx, Vy, nibble.go
@@ -7,25 +7,26 @@ package VAR
 // VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of
 // the display, it wraps around to the opposite side of the screen.
 func DRW_Vx_Vy_nibble(b uint16) {
-	var tmps []uint8
-	for i := CHIP8.Cpu.I; i < CHIP8.Cpu.I+b&0x000F; i++ {
-		tmps = append(tmps, CHIP8.Cpu.Memory[i])
-	}
-	for i := uint8(0); i < uint8(len(tmps)); i++ {
-		for j := uint8(0); j < 8; j++ {
-			CHIP8.Screen.Mapscreen[(CHIP8.Cpu.V[(b&0x0F00)>>8]+j)%64][(CHIP8.Cpu.V[(b&0x00F0)>>4]+i)%32] ^= tmps[i] >> (7 - j) & 0x01
-		}
-	}
-	//xval := CHIP8.Cpu.V[b&0x0F00>>8]
-	//yval := CHIP8.Cpu.V[b&0x00F0>>4]
-	//CHIP8.Cpu.V[0xF] = 0
-	//var i byte = 0
-	//for ; i < byte(b&0xF000); i++ {
-	//	row := CHIP8.Cpu.Memory[CHIP8.Cpu.I+uint16(i)]
-	//	if erased := DrawSprite(xval, yval+i, row); erased {
-	//		CHIP8.Cpu.V[0xF] = 1
+	//var tmps []uint8
+	//for i := CHIP8.Cpu.I; i < CHIP8.Cpu.I+b&0x000F; i++ {
+	//	tmps = append(tmps, CHIP8.Cpu.Memory[i])
+	//}
+	//for i := uint8(0); i < uint8(len(tmps)); i++ {
+	//	for j := uint8(0); j < 8; j++ {
+	//		CHIP8.Screen.Mapscreen[(CHIP8.Cpu.V[(b&0x0F00)>>8]+j)%64][(CHIP8.Cpu.V[(b&0x00F0)>>4]+i)%32] ^= tmps[i] >> (7 - j) & 0x01
 	//	}
 	//}
+
+	xval := CHIP8.Cpu.V[b&0x0F00>>8]
+	yval := CHIP8.Cpu.V[b&0x00F0>>4]
+	CHIP8.Cpu.V[0xF] = 0
+	var i byte = 0
+	for ; i < byte(b&0x000F); i++ {
+		row := CHIP8.Cpu.Memory[CHIP8.Cpu.I+uint16(i)]
+		if erased := DrawSprite(xval, yval+i, row); erased {
+			CHIP8.Cpu.V[0xF] = 1
+		}
+	}
 
 }
 
