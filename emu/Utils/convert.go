cc9e2ec9b270ce3e5283c518fdf2688cc5ee1db8
BaptSoulier <baptiste.soulier@ynov.com>
2023-10-05 13:22:10 +0200

    flags -> 4

--- emu/Utils/convert.go
+++ emu/Utils/convert.go
@@ -1,17 +1,17 @@
 package Utils
 
 func Uint8ToUint16(a uint8, b uint8) uint16 {
-	return uint16(a)<<8 | uint16(b)
+	return uint16(a)<<8 | uint16(b) //8 bits vers la gauche
 }
 
 func Uint16ToUint8(a uint16) (uint8, uint8) {
-	return uint8(a >> 8), uint8(a & 0x00FF)
+	return uint8(a >> 8), uint8(a & 0x00FF) //8 bits vers la droite
 }
 
 func Uint8ToUint4(a uint8) (uint8, uint8) {
-	return a >> 4, a & 0x0F
+	return a >> 4, a & 0x0F //4 bits vers la droite
 }
 
 func Uint4ToUint8(a uint8, b uint8) uint8 {
-	return uint8(a)<<4 | uint8(b)
+	return uint8(a)<<4 | uint8(b) //4 bits vers la gauche
 }
